a0d2c0393393df787810654769ca456ba5ccea69
JJWallace <joshuajohnwallace@gmail.com>
2020-04-22 00:56:22 +1000

    Working tests

--- src/main/java/jjw/project/sudoku/Solver.java
+++ src/main/java/jjw/project/sudoku/Solver.java
@@ -1,17 +1,87 @@
 package jjw.project.sudoku;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import lombok.Data;
 
 public @Data class Solver {
 	
-	private int[][] board;
+	private  int[][] board= new int[9][9];
+	private int[][] solution = new int[9][9];
 
-	public Solver() {
-		board = new int[9][9];
+	public Solver() { 
 		for(int[] temp : board)
 			Arrays.fill(temp, 0);
+	}
+
+
+	public boolean checkRow(int row, int value) {
+		for(int i = 0; i<9; i++) {
+			if(board[row][i] == value)
+				return true;
+		}
+		return false;
+	}
+
+	public Boolean checkColumn(int column, int value) {
+		for(int rowPosition = 0; rowPosition<9; rowPosition++) {
+			if(board[rowPosition][column] == value)
+				return true;
+		}
+		return false;
+	}
+	
+	public Boolean checkSection(int row, int column, int value) {
+		int sectionStartrow = ((row)/3)*3;
+		int sectionStartColumn = ((column)/3)*3;
+		for(int rowPosition = 0; rowPosition < 3;rowPosition++) {
+			for(int columnPosition = 0; columnPosition < 3;columnPosition++)
+				if(board[sectionStartrow + rowPosition][sectionStartColumn + columnPosition] == value)
+					return true;						
+		}
+		return false;
+	}
+
+	public int[][] solution() {
+		solve();
+		return solution;
+	}
+
+
+	private void solve() {
+		for(int rowPosition = 0; rowPosition < 9; rowPosition ++) 
+			for(int columnPosition = 0; columnPosition < 9; columnPosition++) 
+				if(board[rowPosition][columnPosition] == 0) {
+					for(int value = 1; value < 10; value++) 
+						if(possible(rowPosition, columnPosition, value) ) {
+							board[rowPosition][columnPosition] = value;
+							solve();
+							board[rowPosition][columnPosition] = 0;
+													
+						}
+					return;
+				}
+		
+		for(int rowPosition = 0; rowPosition < 9; rowPosition ++) 
+			for(int columnPosition = 0; columnPosition < 9; columnPosition++)
+				solution[rowPosition][columnPosition] = board[rowPosition][columnPosition];
+
+	}
+
+
+	private boolean possible(int rowPosition, int columnPosition, int value) {
+		return (!checkRow(rowPosition, value) && !checkColumn(columnPosition, value) && !checkSection(rowPosition, columnPosition, value));
+	}
+
+
+	private boolean unfinished() {
+		for(int [] tempRow: board)
+			for(int tempNumber: tempRow)
+				if(tempNumber == 0)
+					return true;
+		return false;
 	}
 	
 	
--- src/test/java/jjw/projects/sudoku/tests/TestSolver.java
+++ src/test/java/jjw/projects/sudoku/tests/TestSolver.java
@@ -4,20 +4,82 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import jjw.project.sudoku.Solver;
 
 class TestSolver {
+	private static int[][] testBoard ;
+	Solver testSolver = new Solver();
+	
+	@BeforeEach
+	void setup() {			
+	int[][] testPuzzle = {	{0,9,6,0,4,0,0,3,0},
+							{0,5,7,8,2,0,0,0,0},
+							{1,0,0,9,0,0,5,0,0},
+							{0,0,9,0,1,0,0,0,8},
+							{5,0,0,0,0,0,0,0,2},
+							{4,0,0,0,9,0,6,0,0},
+							{0,0,4,0,0,3,0,0,1},
+							{0,0,0,0,7,9,2,6,0},
+							{0,2,0,0,5,0,9,8,0}};
+	testBoard = testPuzzle;
+	}
 
 	@Test
 	void testBoardExists() {
-		int[][] testBoard = {{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},
-							{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},};
-		Solver testSolver = new Solver();
+		int[][] emptyTestBoard = {	{0,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0}};
 		
-		assertTrue(Arrays.deepEquals(testBoard, testSolver.getBoard()));	
+		assertTrue(Arrays.deepEquals(emptyTestBoard, testSolver.getBoard()));	
 				
 	}
+	@Test
+	void testCheckRow() {
+		testSolver.setBoard(testBoard);
+		assertFalse(testSolver.checkRow(0 , 2));
+		assertTrue(testSolver.checkRow(0, 9));
+	}
+	@Test
+	void testCheckColumn() {
+		testSolver.setBoard(testBoard);
+		assertFalse(testSolver.checkColumn(3 , 1));
+		assertTrue(testSolver.checkColumn(0, 1));
+	}
+	@Test
+	void testCheckSection() {
+		testSolver.setBoard(testBoard);
+		assertFalse(testSolver.checkSection(0, 3, 1));
+		assertTrue(testSolver.checkSection(0, 0, 5));
+		assertTrue(testSolver.checkSection(2,5,4));
+	}
+
+	
+	@Test
+	void testCheckSolution() {
+		testSolver.setBoard(testBoard);
+		int[][] solution = {{2,9,6,1,4,5,8,3,7},
+							{3,5,7,8,2,6,1,4,9},
+							{1,4,8,9,3,7,5,2,6},
+							{6,3,9,5,1,2,4,7,8},
+							{5,8,1,7,6,4,3,9,2},
+							{4,7,2,3,9,8,6,1,5},
+							{9,6,4,2,8,3,7,5,1},
+							{8,1,5,4,7,9,2,6,3},
+							{7,2,3,6,5,1,9,8,4}};
+		int[][] testSolution = testSolver.solution();
+		assertArrayEquals(solution, testSolution, "Nope");
+	}
+	
+	
+
 
 }
